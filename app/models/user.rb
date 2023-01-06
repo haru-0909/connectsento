07af16986ba2dd1ecfea01236572dbd6908ef252
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2023-01-06 16:15:29 +0000

    [Add]ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def self.guest
+      find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = 'サンプル'
+      end
+  end
 end
