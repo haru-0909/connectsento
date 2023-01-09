269250f1d70c8e857e35bc857241f4374f3e4d2b
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2023-01-09 16:17:47 +0000

    [Add]投稿一覧実装途中

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,24 @@
 class Public::PostsController < ApplicationController
 
   def new
-      @post = Post.new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  def index
+    @posts = Post.all
+    @user = current_user
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about', as: :about
-    resources :posts, only: [:new]
+    resources :posts, only: [:new, :create, :index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
