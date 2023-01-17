f4aa6a860346b19ec7501db5147fe4c4429bc278
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2023-01-17 07:07:03 +0000

    [Add]管理者の会員詳細ページ

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,9 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
 end
