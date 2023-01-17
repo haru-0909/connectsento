eb2b7579a39c038ad0d82f4c9bc2e6ea65395caa
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2023-01-17 06:48:34 +0000

    [Add]管理者の会員一覧ページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    if resource_or_scope.is_a?(Admin)
+      admin_users_path
+    else
+      about_path
+    end
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  namespace :admin do
+    resources :users, only: [:index]
+  end
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
